bbd15898c0b239c1a21bdc7a3f244b5ecdb83c0a
Kamich-GP <you@example.com>
2024-08-30 18:18:23 +0500

    frontend

--- index/views.py
+++ index/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 
 # Create your views here.
+# Главная страница
 def home_page(request):
-    return render(request, 'home.html')
+    # Достаем данные из БД
+    all_products = Product.objects.all()
+    all_categories = Category.objects.all()
+    # Передаем данные на фронт
+    context = {'products': all_products, 'categories': all_categories}
+    # Прогружаем html файл
+    return render(request, 'home.html', context)
