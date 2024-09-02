4b43283017bc6d9f12b61b0cd9900ea7b03e1136
Kamich-GP <you@example.com>
2024-09-02 19:06:39 +0500

    forms

--- index/urls.py
+++ index/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home_page),
     path('product/<int:pk>', views.product_page),
-    path('category/<int:pk>', views.category_page)
+    path('category/<int:pk>', views.category_page),
+    path('search', views.search_product),
+    path('register', views.Register.as_view()),
+    path('logout', views.logout_view)
 ]
--- index/views.py
+++ index/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product, Category
+from .forms import SearchForm, RegisterForm
+from django.contrib.auth.models import User
+from django.views import View
+from django.contrib.auth import logout, login
 
 
 # Create your views here.
@@ -8,8 +12,10 @@ def home_page(request):
     # Достаем данные из БД
     all_products = Product.objects.all()
     all_categories = Category.objects.all()
+    # Достаем форму поиска
+    search = SearchForm()
     # Передаем данные на фронт
-    context = {'products': all_products, 'categories': all_categories}
+    context = {'products': all_products, 'categories': all_categories, 'form': search}
     # Прогружаем html файл
     return render(request, 'home.html', context)
 
@@ -34,3 +40,45 @@ def product_page(request, pk):
     context = {'product': product}
     # Прогружаем html файл
     return render(request, 'product.html', context)
+
+
+def search_product(request):
+    if request.method == 'POST':
+        get_product = request.POST.get('search_product')
+
+        if get_product:
+            product = Product.objects.get(pr_name__icontains=get_product)
+            return redirect(f'/product/{product.id}')
+        else:
+            return redirect('/')
+
+
+# Регистрация
+class Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        context = {'form': RegisterForm}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = RegisterForm(request.POST)
+
+        # Если данные корректны
+        if form.is_valid():
+            username = form.clean_username()
+            password2 = form.cleaned_data.get('password2')
+            email = form.cleaned_data.get('email')
+            user = User.objects.create_user(username=username, password=password2,
+                                            email=email)
+            user.save()
+            login(request, user)
+            return redirect('/')
+        # Если данные не совпали
+        context = {'form': RegisterForm}
+        return render(request, self.template_name, context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
