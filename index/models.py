8c32a69f9ed927d7cbd7e07dc11af888d0182fe5
Kamich-GP <you@example.com>
2024-09-04 19:14:59 +0500

    final front

--- index/models.py
+++ index/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     pr_des = models.TextField(blank=True)
     pr_price = models.FloatField()
     pr_count = models.IntegerField()
-    pr_photo = models.ImageField(upload_to='static/pr_img')
+    pr_photo = models.ImageField(upload_to='media')
     pr_category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
