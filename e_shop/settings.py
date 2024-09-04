56c67dd43ea9cae8d7ac287667914b64cac3b3e0
Kamich-GP <you@example.com>
2024-09-04 17:54:10 +0500

    front

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -117,7 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
+import os
+STATICFILES_DIRS = (os.path.join(BASE_DIR / 'assets',),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
